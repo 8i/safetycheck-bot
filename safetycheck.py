06a82e1516418ec6caaa65c66fe278197effd01f
Joel Pitt <joel@8i.com>
2017-03-27 15:44:16 +1300

    Logging for duplicate checks

--- safetycheck.py
+++ safetycheck.py
@@ -56,6 +56,9 @@ def safetycheck():
     # Make sure the old safety check is at least an hour old before overwriting
     if request.form['channel_id'] in checks:
         check = checks[request.form['channel_id']]
+        print("Previous check exists from " + str(check['check_message_ts']))
+        print("Current time is " + str(time.time()))
+        print("Cool down time time is " + str(check_cool_down))
         if int(time.time() - check['check_message_ts']) < check_cool_down:
             return jsonify({
                 "text": "Safety check already in progress!",
